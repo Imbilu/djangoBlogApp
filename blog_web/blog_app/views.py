f7785eaf73ce04bdcdfe35fed42ad241abc26958
Imbilu <imbilusydney09@gmail.com>
2024-02-26 12:39:53 +0300

    created post model

--- blog_web/blog_app/views.py
+++ blog_web/blog_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog_app/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog_app/home.html', context)
 
 def about(request):
     return render(request, 'blog_app/about.html', {'title': 'About'})
